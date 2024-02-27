041a1bd1887d93070a9105e455edb2d45f598e95
1drivewithop <161212729+1drivewithop@users.noreply.github.com>
2024-02-27 20:42:33 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -31,20 +31,24 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double quotes - changing " to \".
   // You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-  'Powered by <a href="https://github.com/iRedScarf/onedrive-index" target="_blank" rel="noopener noreferrer">onedrive-index</a>.',
+  'Powered by <a href="https://t.me/Ifazee" target="_blank" rel="noopener noreferrer"> Faizan</a>. & Made by  <a href="https://t.me/Erroroo" target="_blank" rel="noopener noreferrer"> ❤️ OPGOHIL',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/iRedScarf/onedrive-index',
+      name: 'Telegram',
+      link: 'https://t.me/Erroroo',
+    },
+    {
+      name: 'WHATSAPP',
+      link: 'https://whatsapp.com/channel/0029VaQez1SCRs1rG0JXZB0U',
     },
   ],
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 200,
+  maxItems: 2000,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.
